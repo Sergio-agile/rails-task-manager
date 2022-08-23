9fe632476dd79659ef087576fc0775dbcea2d6e5
sergio garcia <sergio.garcia.sanjuan@gmail.com>
2022-08-23 15:42:10 +0200

    updating task correctly

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'tasks', to: 'tasks#index', as: :tasks
-  get 'tasks/new', to: 'tasks#new', as: :task_new
+  get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
-  get 'tasks/:id', to: 'tasks#show', as: :task_show
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
 end
