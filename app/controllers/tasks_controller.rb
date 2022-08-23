ba72cdc19b8d2dd5c7216d99c1be3195a77942d5
sergio garcia <sergio.garcia.sanjuan@gmail.com>
2022-08-23 14:28:35 +0200

    create task implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,23 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
