9d9f42b3e1e10e4c2f8e10e851bfc7d360ce7f1f
sergio garcia <sergio.garcia.sanjuan@gmail.com>
2022-08-23 16:13:42 +0200

    refactoring with resources, set_task and render the form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
@@ -14,21 +16,21 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
+    set_task
   end
 
   def edit
-    @task = Task.find(params[:id])
+    set_task
   end
 
   def update
-    @task = Task.find(params[:id])
+    set_task
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    set_task
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
@@ -37,5 +39,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
